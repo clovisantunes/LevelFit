745d6b1064f882a6002165a4bbe920177c08bffe
Clovis Antunes <clovissantannaa@gmail.com>
2025-11-19 17:06:10 -0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,16 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
         name: 'LevelFit',
         short_name: 'LevelFit',
+        description: 'LevelFit - Treine como se estivesse em uma aventura.',
+        theme_color: '#6d28d9',
+        background_color: '#000000',
+        display: 'standalone',
         start_url: '/',
         scope: '/',
-        display: 'standalone',
-        background_color: '#000000',
-        theme_color: '#6d28d9',
         icons: [
           {
             src: '/icons/pwa-192x192.png',
@@ -25,11 +27,14 @@ export default defineConfig({
             src: '/icons/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
       }
     })
   ]
